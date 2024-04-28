3fc1726ef1136fafda73b13891b5d2fd6ab80c82
emanuelquintanilla85@gmail.com <emanuelquintanilla85@gmail.comm>
2024-04-28 01:20:38 -0300

    fix: los datos a mostrar desde el buttons,

--- clickcat/src/components/ui/Buttons.tsx
+++ clickcat/src/components/ui/Buttons.tsx
@@ -1,65 +1,17 @@
 import { propsButton } from '@/utils/type'
 import React from 'react'
 import Style from './ui.module.css'
-import { Tooltip, Typography } from "@material-tailwind/react";
 
-export const Buttons = ({ children, toolTipDescription, toolTipValueProbability, toolTipTotalProbability, ...props }: propsButton,) => {
+export const Buttons = ({ children, ...props }: propsButton,) => {
 
   return (
     <div>
-      <Tooltip
-        content={
-          <div className="w-full">
-            <Typography
-              color="white"
-              className="font-normal opacity-80"
-            >
-              {`${toolTipDescription}`}
-            </Typography>
-            <div className='flex flex-row'>
-              <Typography
-                color="white"
-                className="font-normal opacity-80 pr-1"
-              >
-                Probabilidad
-              </Typography>
-              <Typography
-                color="white"
-                className=" font-bold opacity-80 text-pink-400"
-              >
-                {`${toolTipValueProbability}`}
-              </Typography>
-            </div>
-            <div className='flex flex-row'>
-              <Typography
-                color="white"
-                className="font-normal opacity-80 pr-1"
-              >
-                Total
-              </Typography>
-              <Typography
-                color="white"
-                className=" font-bold opacity-80 text-pink-400"
-              >
-                {`${toolTipTotalProbability}`}
-              </Typography>
-            </div>
-
-          </div>}
-        animate={{
-          mount: { scale: 0.9, x: 10 },
-          unmount: { scale: 0, y: 0 },
-        }}
-        className="bg-indigo-900 py-2 px-3 text-white"
-        placement="left-center"
+      <button
+        className={Style.button}
+        {...props}
       >
-        <button
-          className={Style.button}
-          {...props}
-        >
-          {children}
-        </button>
-      </Tooltip>
+        {children}
+      </button>
     </div>
   )
 }
