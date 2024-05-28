f43837f468bcf38e83738dc1bee985f7a7b30714
emanuelquintanilla85@gmail.com <emanuelquintanilla85@gmail.comm>
2024-05-28 00:16:41 -0300

    delete: archivos eliminados

--- clickcat/src/Modules/Buildings/hooks/changeBuildingCost.tsx
+++
@@ -1,7 +0,0 @@
-import { BuildingsObject } from "@/utils/type";
-
-export default function changeBuildingCost(building: BuildingsObject) {
-    let result = building.buildingCost;
-    result = result + 2
-    return result;
-}
